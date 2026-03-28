591799c2b505439a9addde98d0cd6d8bc26d6c5b
muxlisa-maxmudova <akobirurozov22@gmail.com>
2026-03-28 09:18:11 +0500

    scalar arithmatic

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 import numpy as np
-# Slicing
+# Scalar arithmetic
 
-array = np.array([[1,2,3,4],
-                  [5,6,7,8],
-                  [9,10,11,12],
-                  [13,14,15,16]])
-
-# array[start:end:step]
-
-print(array[:2, 2:]) #first 2 rows and 2 last columns
+array = np.array([1,2,3])
+print(array**3)
