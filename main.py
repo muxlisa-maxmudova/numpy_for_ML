11a88c28d49b55beaaa04407132a3b7b203b43cc
muxlisa-maxmudova <akobirurozov22@gmail.com>
2026-03-28 13:07:52 +0500

    filtering through indexing

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import numpy as np
+# Filtering
 
-array = np.array([[1,2,3,4,5],
-                  [6,7,8,9,10]])
-print(np.sum(array, axis = 1))
+ages = np.array([[21, 17, 19, 20, 16, 30, 18, 65],
+                 [39, 22, 15, 99, 18, 19, 20, 21]])
+# we can filter out elements that don't match
+# our conditions
+
+teenagers = ages[ages < 18]
+adults = ages[(ages>=18) & (ages<65)]
+seniors = ages[ages >= 65]
+evens = ages[ages % 2 == 0]
+odds = ages[ages%2!=0]
+print(odds)
+
 
 
 
