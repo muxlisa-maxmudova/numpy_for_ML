683105002ace941384fe731780e4c1e7110ffd5f
muxlisa-maxmudova <akobirurozov22@gmail.com>
2026-03-28 09:30:36 +0500

    adding arrays by elements

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import numpy as np
-# Vectorized math function
-
-radii = np.array([1,2,3])
-
-print(np.pi * radii**2) # fun exercise
+# Element - wise arithmatic
+array1 = np.array([1,2,3])
+array2 = np.array([4,5,6])
+print(array1**array2)
