5a8025465d344fc2f5c701b60c8d09168baa1ea2
muxlisa-maxmudova <akobirurozov22@gmail.com>
2026-03-28 08:48:30 +0500

    multidimensional arrays - fun exercise

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ import numpy as np
 array = np.array([[["A", "B", "C"],["D", "E", "F"], ["G", "H", "I"]],
                   [["J", "K", "L"],["M", "N", "O"], ["P", "Q", "R"]],
                   [["S", "T", "U"],["V", "W", "X"], ["Y", "Z", ""]]])
-print(array[0,0,0]) # indexing with numpy
+word = array[0,2,1] + array[0,0,0] + array[2,0,0] + array[0,0,0] + array[1,1,1]
+
+print(word)
