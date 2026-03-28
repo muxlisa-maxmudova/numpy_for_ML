a8a8b47a15007d2b687e46b645409af8d02ab7f2
muxlisa-maxmudova <akobirurozov22@gmail.com>
2026-03-28 08:31:06 +0500

    multidimensional arrays

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import numpy as np
-#1. Creating numpy list:
-array = np.array([1,2,3,4])
-print(array)
-
-#2. To verify whether it is numpy list:
-print(type(array))
+# Determining dimension of the array:
+array = np.array("A")
+print(array.ndim)
+# Output is 0
