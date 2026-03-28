f2ac541246d297f7e4996b27dbb62c0c7ebec96b
muxlisa-maxmudova <akobirurozov22@gmail.com>
2026-03-28 15:42:07 +0500

    slicing activity

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import numpy as np
-# Shuffling the array
-rng = np.random.default_rng()
-fruits = np.array(["apple", "orange", "coconut", "pineapple"])
-fruit = rng.choice(fruits, size = (2,2))
-print(fruit)
 
+data = np.array([
+    [10, 95, 85],
+    [2,  40, 30],
+    [8,  80, 75],
+    [5,  60, 55]
+])
 
-
-
+# Task 1:
+attendance_and_study_hours = data[:4, :2]
+print(attendance_and_study_hours)
+#Task 2:
+final_grades = data[:, -1]
+print(final_grades)
+#Task 3:
+top_student = data[0,:]
+print(top_student)
 
 
 
