cc90c770ac6d99bde76ca59787f06831f7fc012b
muxlisa-maxmudova <akobirurozov22@gmail.com>
2026-03-28 13:19:07 +0500

    UNIFORM function

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import numpy as np
 # Random numbers
-
-rng = np.random.default_rng(seed = 1) # same numbers appear everytime
-print(rng.integers(low =1, high = 101, size = (3, 2))) # high (second number) is exclusive so we generate numbers 1, 2, 3, 4, 5 and 6
-
+np.random.seed(seed=1)
+print(np.random.uniform(low = -1, high = 1, size = (3,2)))
 
 
 
