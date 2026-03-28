2586849e23603be3931f953e1f16d26ad33f764b
muxlisa-maxmudova <akobirurozov22@gmail.com>
2026-03-28 11:22:12 +0500

    What do I mean by aggregating functions?

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 import numpy as np
-# Broadcasting
+# Aggregate functions => summarize data and
+# typically return a single value
 
-#Exercise
 
-array1 = np.array([[1,2,3,4,5,6,7,8,9,10]])
-array2 = np.array([[1], [2], [3], [4], [5], [6], [7], [8], [9], [10]])
 
-print(array1.shape)
-print(array2.shape)
-print(array1*array2)
+
 
 
 
