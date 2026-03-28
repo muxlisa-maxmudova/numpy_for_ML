ec26cf31c4e534d8c98d51075526707d0bc60170
muxlisa-maxmudova <akobirurozov22@gmail.com>
2026-03-28 11:40:34 +0500

    statistics functions

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import numpy as np
-# Aggregate functions => summarize data and
-# typically return a single value
+
+array = np.array([[1,2,3,4,5], [6,7,8,9,10]])
+print(np.sum(array))
+print(np.mean(array))
+print(np.var(array))
+print(np.argmax(array))
 
 
 
