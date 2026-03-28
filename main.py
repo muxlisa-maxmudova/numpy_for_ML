99f422449917c40a24799f10862012b113feaf25
muxlisa-maxmudova <akobirurozov22@gmail.com>
2026-03-28 08:42:15 +0500

    multidimensional arrays - chain indexing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
-# Determining dimension of the array:
-array = np.array("A")
-print(array.ndim)
-# Output is 0
+# Operations on multidimensional array:
+array = np.array([[["A", "B", "C"],["D", "E", "F"], ["G", "H", "I"]],
+                  [["J", "K", "L"],["M", "N", "O"], ["P", "Q", "R"]],
+                  [["S", "T", "U"],["V", "W", "X"], ["Y", "Z", ""]]])
+print(array[0][0][0]) # chain indexing - regular
