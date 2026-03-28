b95cbf056f43885b7f8fd7c4f8420c0789772e26
muxlisa-maxmudova <akobirurozov22@gmail.com>
2026-03-28 12:59:48 +0500

    summing using axis

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import numpy as np
 
-array = np.array([[1,2,3,4,5], [6,7,8,9,10]])
-print(np.sum(array))
-print(np.mean(array))
-print(np.var(array))
-print(np.argmax(array))
+array = np.array([[1,2,3,4,5],
+                  [6,7,8,9,10]])
+print(np.sum(array, axis = 1))
+
 
 
 
