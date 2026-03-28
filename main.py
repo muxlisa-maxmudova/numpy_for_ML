7646d0c24745f4cab6067fd5c7f4d2088cd636ea
muxlisa-maxmudova <akobirurozov22@gmail.com>
2026-03-28 11:15:08 +0500

    they are not compatible, why?

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import numpy as np
 # Broadcasting
 
-# Broadcasting allows NumPy to perform operations
-# on arrays with different shapes by virtually
-# expanding dimensions so they match the larger
-# array's shape.
+array1 = np.array([[1,2,3,4],
+                   [5,6,7,8],
+                   [9,10,11,12],
+                   [13,14,15,16]])
+array2 = np.array([[1,2],[2,3],[3,4],[4,5]])
+print(array1.shape)
+print(array2.shape)
 
-# The dimensions have the same size.
-# OR
-# One of the dimensions has a size of 1.
+print(array1*array2)
+
 
 
 
