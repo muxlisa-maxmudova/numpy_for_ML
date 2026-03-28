8cecc22cf56107a29a03963109a334163e36ebef
muxlisa-maxmudova <akobirurozov22@gmail.com>
2026-03-28 11:19:55 +0500

    creating multiplication table using broadcasting

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import numpy as np
 # Broadcasting
 
-array1 = np.array([[1,2,3,4],
-                   [5,6,7,8],
-                   [9,10,11,12],
-                   [13,14,15,16]])
-array2 = np.array([[1,2],[2,3],[3,4],[4,5]])
+#Exercise
+
+array1 = np.array([[1,2,3,4,5,6,7,8,9,10]])
+array2 = np.array([[1], [2], [3], [4], [5], [6], [7], [8], [9], [10]])
+
 print(array1.shape)
 print(array2.shape)
-
 print(array1*array2)
+
+
+
 
 
 
