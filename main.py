48d4453e3aae4ba08af3511ff22aabf4f3b05dc7
muxlisa-maxmudova <akobirurozov22@gmail.com>
2026-03-28 13:10:25 +0500

    filtering using where function

--- main.py
+++ main.py
@@ -3,15 +3,8 @@ import numpy as np
 
 ages = np.array([[21, 17, 19, 20, 16, 30, 18, 65],
                  [39, 22, 15, 99, 18, 19, 20, 21]])
-# we can filter out elements that don't match
-# our conditions
-
-teenagers = ages[ages < 18]
-adults = ages[(ages>=18) & (ages<65)]
-seniors = ages[ages >= 65]
-evens = ages[ages % 2 == 0]
-odds = ages[ages%2!=0]
-print(odds)
+adults = np.where(ages >= 18, ages, 0) # retains the original shape, any element that doesn't match the condition replaced by 0
+print(adults)
 
 
 
